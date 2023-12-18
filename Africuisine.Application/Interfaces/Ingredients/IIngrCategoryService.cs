caff374a37bcdaa6bce39c343a232f6343a5d43a
Seoka Moshele Kabelo <seokamk@gmail.com>
2023-12-18 22:54:08 +0200

    Ingredient Category Service Implemented

--- Africuisine.Application/Interfaces/Ingredients/IIngrCategoryService.cs
+++ Africuisine.Application/Interfaces/Ingredients/IIngrCategoryService.cs
@@ -5,5 +5,7 @@ namespace Africuisine.Application.Interfaces.Ingredients;
 public interface IIngrCategoryService { 
     Task<PostResponse> Create(CreateCategoryCommand command);
     Task<QueryItemResponse<IngredientCategoryDTO>> GetIngredientCategoryById(string id);
-    Task<QueryItemsResponse<IngredientCategoryDTO>> GetIngredientCategories();
+    Task<QueryItemsResponse<IngredientCategoryDTO>> GetIngredientCategories(int page,
+        int size);
+    
 }
--- Africuisine.Infrastructure/Services/Ingredients/IngrCategoryService.cs
+++ Africuisine.Infrastructure/Services/Ingredients/IngrCategoryService.cs
@@ -1,23 +1,59 @@
 ﻿using Africuisine.Application;
 using Africuisine.Application.Interfaces.Ingredients;
 using Africuisine.Application.Res;
+using Africuisine.Domain.Ingredients;
+using Africuisine.Infrastructure.Context;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Africuisine.Infrastructure;
 
 public class IngrCategoryService : IIngrCategoryService
 {
-    public Task<PostResponse> Create(CreateCategoryCommand command)
+    private readonly IMapper Mapper;
+    private readonly IngredientDBContext Data;
+
+    public IngrCategoryService(IMapper mapper, IngredientDBContext data)
     {
-        throw new NotImplementedException();
+        Mapper = mapper;
+        Data = data;
     }
 
-    public Task<QueryItemsResponse<IngredientCategoryDTO>> GetIngredientCategories()
+    public async Task<PostResponse> Create(CreateCategoryCommand command)
     {
-        throw new NotImplementedException();
+        var category = Mapper.Map<IngredientCategoryDM>(command);
+        Data.IngredientCategories.Add(category);
+        int rows = await Data.SaveChangesAsync();
+        return new PostResponse { Succeeded = rows > 0 };
     }
 
-    public Task<QueryItemResponse<IngredientCategoryDTO>> GetIngredientCategoryById(string id)
+    public async Task<QueryItemsResponse<IngredientCategoryDTO>> GetIngredientCategories(
+        int page,
+        int size
+    )
     {
-        throw new NotImplementedException();
+        var categories = await Data.IngredientCategories
+            .ProjectTo<IngredientCategoryDTO>(Mapper.ConfigurationProvider)
+            .Skip((page - 1) * size)
+            .Take(size)
+            .ToListAsync();
+        return new QueryItemsResponse<IngredientCategoryDTO>
+        {
+            Items = categories,
+            Succeeded = categories.Count > 0
+        };
+    }
+
+    public async Task<QueryItemResponse<IngredientCategoryDTO>> GetIngredientCategoryById(string id)
+    {
+        var category = await Data.IngredientCategories
+            .ProjectTo<IngredientCategoryDTO>(Mapper.ConfigurationProvider)
+            .FirstOrDefaultAsync(x => x.Id == id);
+        return new QueryItemResponse<IngredientCategoryDTO>
+        {
+            Item = category,
+            Succeeded = category is not null
+        };
     }
 }
